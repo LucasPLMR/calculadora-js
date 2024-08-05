07a1fdd1cef1c4f7b8ed97cff9b864e94e789e0e
LucasPLMR <115842611+LucasPLMR@users.noreply.github.com>
2024-08-05 19:21:50 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,102 @@
+const main = document.querySelector("main");
+const root = document.querySelector(":root");
+const input = document.getElementById("input");
+const resultInput = document.getElementById("result");
+
+// Teclas que são permitidas no input
+const allowedKeys = [
+  "(",
+  ")",
+  "/",
+  "*",
+  "-",
+  "+",
+  "9",
+  "8",
+  "7",
+  "6",
+  "5",
+  "4",
+  "3",
+  "2",
+  "1",
+  "0",
+  ".",
+  "%",
+  " ",
+];
+
+// Adicionando valor aos botões
+document.querySelectorAll(".charKey").forEach(function (charKeyBtn) {
+  charKeyBtn.addEventListener("click", function () {
+    const value = charKeyBtn.dataset.value;
+    input.value += value;
+  });
+});
+
+// Botão clear(C)
+document.getElementById("clear").addEventListener("click", function () {
+  input.value = "";
+  input.focus();
+});
+
+// Adicionando valor as teclas
+input.addEventListener("keydown", function (ev) {
+  ev.preventDefault();
+  if (allowedKeys.includes(ev.key)) {
+    input.value += ev.key;
+    return;
+  }
+  if (ev.key === "Backspace") {
+    input.value = input.value.slice(0, -1);
+  }
+  if (ev.key === "Enter") {
+    calculate();
+  }
+});
+
+// Botão de igualar
+document.getElementById("equal").addEventListener("click", calculate);
+
+// função para calcular
+function calculate() {
+  resultInput.value = "ERROR";
+  resultInput.classList.add("error");
+
+  const result = eval(input.value);
+
+  resultInput.value = result;
+  resultInput.classList.remove("error");
+}
+
+// Botão para copiar para a Área de Transferência
+document
+  .getElementById("copyToClipboard")
+  .addEventListener("click", function (ev) {
+    const button = ev.currentTarget;
+    if (button.innerText === "Copy") {
+      button.innerText = "Copied";
+      button.classList.add("success");
+      navigator.clipboard.writeText(resultInput.value);
+    } else {
+      button.innerText = "Copy";
+      button.classList.remove("success");
+    }
+  });
+
+// Botão para alterar Tema
+document.getElementById("themeSwitcher").addEventListener("click", function () {
+  if (main.dataset.theme === "dark") {
+    root.style.setProperty("--bg-color", "#f1f5f9");
+    root.style.setProperty("--border-color", "#aaa");
+    root.style.setProperty("--font-color", "#212529");
+    root.style.setProperty("--primary-color", "#26834a");
+    main.dataset.theme = "light";
+  } else {
+    root.style.setProperty("--bg-color", "#212529");
+    root.style.setProperty("--border-color", "#666");
+    root.style.setProperty("--font-color", "#f1f5f9");
+    root.style.setProperty("--primary-color", "#4dff91");
+    main.dataset.theme = "dark";
+  }
+});
